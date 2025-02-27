327cc60407e51dc31a7c5e7da2ee08df5dc2a14b
Carlistan <carlistan14@gmail.com>
2025-02-27 11:15:42 +0100

    Main

--- src/PrincipalAPP.java
+++ src/PrincipalAPP.java
@@ -1,4 +1,8 @@
-
+import java.util.*;
 public class PrincipalAPP {
-
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> eventos = new ArrayList<>();
+        HashMap<String, ArrayList<String>> inscripciones = new HashMap<>();
+	}
 }
