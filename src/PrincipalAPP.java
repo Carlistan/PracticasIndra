a70c5c11aa0e4b5af656e59d9947d0cec5967b10
Carlistan <carlistan14@gmail.com>
2025-02-27 11:22:50 +0100

    Menu

--- src/PrincipalAPP.java
+++ src/PrincipalAPP.java
@@ -4,5 +4,16 @@ public class PrincipalAPP {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> eventos = new ArrayList<>();
         HashMap<String, ArrayList<String>> inscripciones = new HashMap<>();
+        
+        while (true) {
+            System.out.println("\n1. Agregar evento");
+            System.out.println("2. Listar eventos");
+            System.out.println("3. Inscribirse a un evento");
+            System.out.println("4. Mostrar inscripciones");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción: ");
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+        }
 	}
 }
